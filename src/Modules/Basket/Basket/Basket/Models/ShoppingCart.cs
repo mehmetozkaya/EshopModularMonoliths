6c8be2e525d5b3f2b752d902d7497346cd84b0b2
mehmetozkaya <mehmetozkaya@gmail.com>
2024-07-27 16:12:34 +0300

    sec16

---
+++ src/Modules/Basket/Basket/Basket/Models/ShoppingCart.cs
@@ -0,0 +1,50 @@
+﻿namespace Basket.Basket.Models;
+public class ShoppingCart : Aggregate<Guid>
+{
+    public string UserName { get; private set; } = default!;
+
+    private readonly List<ShoppingCartItem> _items = new();
+    public IReadOnlyList<ShoppingCartItem> Items => _items.AsReadOnly();
+    public decimal TotalPrice => Items.Sum(x => x.Price * x.Quantity);
+
+    public static ShoppingCart Create(Guid id, string userName)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(userName);
+
+        var shoppingCart = new ShoppingCart
+        {
+            Id = id,
+            UserName = userName
+        };
+
+        return shoppingCart;
+    }
+
+    public void AddItem(Guid productId, int quantity, string color, decimal price, string productName)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(quantity);
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+
+        var existingItem = Items.FirstOrDefault(x => x.ProductId == productId);
+
+        if (existingItem != null)
+        {
+            existingItem.Quantity += quantity;
+        }
+        else
+        {
+            var newItem = new ShoppingCartItem(Id, productId, quantity, color, price, productName);
+            _items.Add(newItem);
+        }
+    }
+
+    public void RemoveItem(Guid productId)
+    {
+        var existingItem = Items.FirstOrDefault(x => x.ProductId == productId);
+
+        if (existingItem != null)
+        {
+            _items.Remove(existingItem);
+        }
+    }
+}
---
+++ src/Modules/Basket/Basket/Basket/Models/ShoppingCartItem.cs
@@ -0,0 +1,24 @@
+﻿using Shared.DDD;
+
+namespace Basket.Basket.Models;
+public class ShoppingCartItem : Entity<Guid>
+{
+    public Guid ShoppingCartId { get; private set; } = default!;
+    public Guid ProductId { get; private set; } = default!;
+    public int Quantity { get; internal set; } = default!;
+    public string Color { get; private set; } = default!;
+
+    // will comes from Catalog module
+    public decimal Price { get; private set; } = default!;
+    public string ProductName { get; private set; } = default!;
+
+    internal ShoppingCartItem(Guid shoppingCartId, Guid productId, int quantity, string color, decimal price, string productName)
+    {
+        ShoppingCartId = shoppingCartId;
+        ProductId = productId;
+        Quantity = quantity;
+        Color = color;
+        Price = price;
+        ProductName = productName;
+    }
+}
